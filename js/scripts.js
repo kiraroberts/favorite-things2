237510a2de5958cac4c9f6f3314dd9c71afc2952
Kira Roberts and Marina Poltorak <marina@>
2019-06-10 14:12:31 -0700

    Dri up arrays

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,27 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
-    var colorInput = $("input#color").val();
-    var personInput = $("input#person").val();
-    var animalInput = $("input#animal").val();
-    var songInput = $("input#song").val();
+    // var colorInput = $("input#color").val();
+    // var personInput = $("input#person").val();
+    // var animalInput = $("input#animal").val();
+    // var songInput = $("input#song").val();
 
-    var favorites = [colorInput, personInput, animalInput, songInput];
+    var favorites = ["color", "person", "animal", "song"];
 
-  $("#story").show();
-  $("#display").text(favorites);
+    favorites.forEach(function(blank) {
+      var userInput = $("input#" + blank).val();
+      var arr2 = [];
+      arr2.push(favorites[0], favorites[1], favorites[2], favorites[3]);
+      $("." + blank).text("Your favorite is " + userInput + ".");
+      // $(".color").text("Your favorite color is " + arr2[0] + ".");
+      // $(".person").text("Your favorite person is " + arr2[1] + ".");
+      // $(".animal").text("Your favorite animal is " + arr2[2] + ".");
+      // $(".song").text("Your favorite song is " + arr2[3] + ".");
+    });
 
-  var arr2 = [];
-  arr2.push(favorites[0], favorites[1], favorites[2], favorites[3]);
-  $(".color").text("Your favorite color is " + arr2[0] + ".");
-  $(".person").text("Your favorite person is " + arr2[1] + ".");
-  $(".animal").text("Your favorite animal is " + arr2[2] + ".");
-  $(".song").text("Your favorite song is " + arr2[3] + ".");
 
+    $("#story").show();
 
+    // $("#display").text(favorites);
     event.preventDefault();
   });
 });
